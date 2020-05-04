3f1222f23ebaa5a88f8446aeffa308eaf729db38
Antonio Latimore <italaphor@gmail.com>
2020-05-03 19:07:01 -0700

    Creating includes
    includes PHP files header & footer. created index.php

---
+++ my-site/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <?php include 'include/head-section.html' ?>
+  </head>
+  <body id="cover-page">
+    <div id="container" class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <!-- header -->
+      <?php include 'include/header.html' ?>
+       <!-- / header -->
+        <h1 class="cover-heading d-none">A|L</h1>
+        <p class="lead">Life experience provides opportunity.</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+
+      <!-- footer -->
+      <?php include 'include/footer.html'?>
+      <!-- / footer -->
+    </div>
+    <!-- bootstrap js -->
+    <?php include 'bootstrap-javascript.html'?>
+  </body>
+</html>
