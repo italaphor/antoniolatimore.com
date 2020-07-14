5fa2b9628c5c9fa5b1b80c74352e62fb2ec7cbdf
italaphor <50608763+italaphor@users.noreply.github.com>
2020-07-13 21:15:18 -0700

    Site UI testing

--- my-site/js/javascript.js
+++ my-site/js/javascript.js
@@ -1,5 +1,24 @@
-function hideAndShowpage(){
+/*
+function hidePage(){
     document.getElementById('about').style.display = 'none';
+    document.getElementById('focus').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
+hidePage();
+*/
+/*
+function showPage(){
+    document.getElementById('about').addEventListener('click', style.display = 'block');
+    document.getElementById('focus').addEventListener('click', style.display = 'block');
+    document.getElementById('contact').addEventListener('click', style.display = 'block');
+}
+showPage();
+*/
+
+function showPage(){
+    document.getElementById('about').style.display = 'block';
+    document.getElementById('focus').style.display = 'block';
+    document.getElementById('contact').style.display = 'block';
 }
 
-hideAndShowpage();
+
